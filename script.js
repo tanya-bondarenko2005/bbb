507522131621d355065b32b16031207fef0878d2
tanya-bondarenko2005 <155098453+tanya-bondarenko2005@users.noreply.github.com>
2024-02-23 18:14:34 +0300

    laba 17

--- script.js
+++ script.js
@@ -1,11 +1,107 @@
-$(function() { 
-    // при большом кодве библиотек это делается чтоб не было конфликта имен с этими знаками, запуск в момент загрузки страницы
-    $(document).on('click', 'button.exclusive', function() {
-        let $button = $(this);
-        $button.removeClass('added').addClass('disabled').attr('disabled', 'disabled');
-        // attr это добавление значения стиля в документ
-        setTimeout(function () {
-            $button.removeClass('disabled').addClass('added').removeAttr('disabled');
-        }, 1000);
-    });
+let jsonContent = '{"items": ["Элемент 1", "Элемент 2", "Элемент 3"]}';
+let listData = JSON.parse(jsonContent);
+let ulElement = document.createElement('ul');
+
+listData.items.forEach(function(item) {
+    let liElement = document.createElement('li');
+    liElement.textContent = item;
+    ulElement.appendChild(liElement);
+});
+
+document.body.appendChild(ulElement);
+
+$(document).ready(function() {
+    console.log($('body').children());
+});
+
+
+
+
+
+let tableData = [
+  ["", "Present", "Past", "Future"],
+  ["+", "i am", "i was", "i will"],
+  ["-", "i am not", "i wasnt", "ill not"],
+  ["?", "do i?", "did i", "will i"]
+];
+
+
+let timeDescriptions = {
+  "Present": "Текущее время",
+  "Past": "Прошедшее время",
+  "Future": "Будущее время"
+};
+
+
+$(document).ready(function() {
+
+  let table = $('<table>').addClass('my-table');
+
+
+  tableData.forEach(function(rowData, rowIndex) {
+      let row = $('<tr>').appendTo(table);
+
+      rowData.forEach(function(cellData, columnIndex) {
+          let cell = $('<td>').text(cellData).appendTo(row);
+
+          
+          if (rowIndex === 0 || columnIndex === 0) {
+              cell.addClass('salad-color');
+
+             
+              cell.hover(function() {
+                
+                  let tooltip = $('#tooltip');
+                  if (!tooltip.length) {
+                      tooltip = $('<div>').attr('id', 'tooltip').appendTo('body');
+                  }
+
+             
+                  tooltip.text(timeDescriptions[cell.text()]);
+
+                  
+                  let offset = cell.offset();
+                //   свойство получения координат относительно отступов родительского элемента
+
+                  tooltip.css({
+                      top: offset.top + cell.outerHeight(),
+                    //   изменение высоты элемента
+                      left: offset.left
+                  });
+              }, function() {
+                  
+                  $('#tooltip').remove();
+              });
+          } else {
+              cell.addClass('turquoise-color');
+          }
+      });
+  });
+
+  $('body').append(table);
+  table.addClass('my-table');
+});
+
+
+$(document).ready(function() {
+ 
+  $('#submitButton').click(function() {
+     
+      let formIsValid = true;
+
+      $('form input').each(function() {
+          if ($(this).val() === '') {
+              formIsValid = false;
+              return false;
+            //    если пустое то ложь
+          }
+      });
+
+      // Вывод предупреждения, если не все поля заполнены
+      if (!formIsValid) {
+          $('#warningMessage').show();
+      } else {
+          $('#warningMessage').hide();
+      }
+  });
 });
